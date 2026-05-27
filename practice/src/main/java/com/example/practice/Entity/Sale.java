4fd2726fd5ccf6f636eab9ae2ab29adbce46a8d3
mokeyType <asmehta667@gmail.com>
2026-05-27 15:56:48 +0530

    deployed database code change

--- practice/src/main/java/com/example/practice/Entity/Sale.java
+++ practice/src/main/java/com/example/practice/Entity/Sale.java
@@ -18,7 +18,7 @@ public class Sale {
     @Column(name = "Sale_Date")
     private LocalDate saleDate;
 
-    @Column(name = "Sale_Time", columnDefinition = "TIME")
+    @Column(name = "Sale_Time")
     private LocalTime saleTime;
 
     @JsonIgnore
